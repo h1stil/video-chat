e6730ca9c8f941e0b0b3e982063c98026cab5d4c
h1stil <jkaem111@gmail.com>
2023-02-11 23:44:22 +0300

    feat: added styles and search for all-users modal

--- src/modules/MainPage/components/AllUsersModal/AllUsersModal.tsx
+++ src/modules/MainPage/components/AllUsersModal/AllUsersModal.tsx
@@ -6,6 +6,7 @@ import "./AllUsersModal.scss";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import { TeamOutlined } from "@ant-design/icons";
+import Contact from "../Contact/Contact";
 import { Input } from "antd";
 
 const AllUsersModal = () => {
@@ -25,14 +26,17 @@ const AllUsersModal = () => {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: "80%",
-    height: "80vh",
+    width: "400px",
+    height: "400px",
     bgcolor: "background.paper",
     p: 4,
     borderRadius: "20px",
+    overflow: "auto",
   };
 
+  const onSearch = (value: string) => value;
   const { Search } = Input;
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <div className="list__all-users">
@@ -47,10 +51,42 @@ const AllUsersModal = () => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
+          <div>
+            <Search
+              className="all-users__search-input"
+              placeholder="Поиск среди пользователей"
+              allowClear
+              onChange={(e) => {
+                onSearch(e.target.value);
+                setInputValue(e.target.value);
+              }}
+              onSearch={onSearch}
+              style={{ width: 298, padding: "5px 10px" }}
+            />
+          </div>
           <div className="all-users__container">
-            {allUsers.map((user) => (
-              <div key={user.id}>{user.name}</div>
-            ))}
+            {allUsers
+              .filter(
+                (filtred) => filtred.name.toLowerCase().indexOf(inputValue) >= 0
+              )
+              .map((user) => (
+                <div
+                  className="all-users__item"
+                  key={user.id}
+                  onClick={() => console.log(user)}
+                >
+                  <Contact
+                    user={{
+                      avatar: user.avatar as string,
+                      id: user.id,
+                      name: user.name,
+                      isOnline: false,
+                    }}
+                    lastMessage={null}
+                    isMe={false}
+                  />
+                </div>
+              ))}
           </div>
         </Box>
       </Modal>
