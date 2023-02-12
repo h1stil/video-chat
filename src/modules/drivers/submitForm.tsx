5af6d0e5dea165a7455f68f7f1211e537360e800
Irina Migunova <ista.irina@gmail.com>
2023-02-13 01:50:19 +0300

    fix: change HTTP to HTTPS

--- src/modules/drivers/submitForm.tsx
+++ src/modules/drivers/submitForm.tsx
@@ -9,7 +9,7 @@ export default async function submitForm(
 
   try {
     const response = await fetch(
-      `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}${path}`,
+      `https://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}${path}`,
       {
         method: "POST",
         headers: {
