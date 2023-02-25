999dd4e329b0e7436db612481d07b7da9613f18b
Irina Migunova <ista.irina@gmail.com>
2023-02-26 01:24:34 +0300

    feat: add greetings to homepage

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,9 +1,20 @@
 import "./StartPage.scss";
+import { greetings } from "../values/greetings";
+
+const ind = Math.floor(Math.random() * greetings.length);
 
 const StartPage = () => {
   return (
     <div className="startpage__mainzone">
-      <img className="startpage__pic" src="../../../001.png"></img>
+      <div className="startpage__picbox">
+        <img src="../../../001.png"></img>
+        <div className="startpage__greetbox">
+          <span className="">{greetings[ind][0]}</span>
+        </div>
+        <div className="startpage__greetcat">
+          <span className="">{greetings[ind][1]}</span>
+        </div>
+      </div>
     </div>
   );
 };
